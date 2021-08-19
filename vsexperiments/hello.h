6f081420860b85538b04a1bbf8b2d4f05e922e68
impasible <88623932+impasible@users.noreply.github.com>
2021-08-19 11:24:19 +0530

    contains header file and the main program

---
+++ vsexperiments/hello.h
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void hello()
+{
+   cout<<"Hello world!\n";
+}
+void yo()
+{
+   cout<<"Yo world!\n";
+}
+void wassup()
+{
+   cout<<"Wassup world!\n";
+}
